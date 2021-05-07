c5002d7123e5425d54815723c448e5ddd8e5b26f
Radmir Abkarimov <Abkarimov2001@outlook.com>
2021-05-07 19:53:31 +0500

    Исправление

--- src/main/java/laba4/MainWindow.java
+++ src/main/java/laba4/MainWindow.java
@@ -19,6 +19,8 @@ public class MainWindow extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		RequestCalc Calc = RequestCalc.fromRequestParameters(request);
 		Calc.setAsRequestAttributesAndCalculate(request);
+		
+		request.getRequestDispatcher("/Form.jsp").forward(request, response);
 	}
 	
 	private static class RequestCalc {
