fe5e0789f856d9572855f56361a75e1971d01fff
Radmir Abkarimov <Abkarimov2001@outlook.com>
2021-05-08 04:15:31 +0500

    Попытка загрузки коэффициентов

--- src/main/java/laba4/UploadDownloadFileConfServlet.java
+++ src/main/java/laba4/UploadDownloadFileConfServlet.java
@@ -1,32 +1,56 @@
 package laba4;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-
+import java.io.InputStream;
+ 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.apache.commons.fileupload.FileItem;
+ 
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
-
-
+ 
 @WebServlet("/UploadDownloadFileServlet")
-@MultipartConfig
 public class UploadDownloadFileConfServlet extends HttpServlet {
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException  {
-		
-		ServletFileUpload = new ServletFileUpload(new DiskFileItemFactory());
-		
-		FileItem file = fileUpload.parseRequest(request);
-		
-		file.write(new File("config.txt"));
-	    
-	    
-	}
-    
+    private static final long serialVersionUID = 1L;
+    @Override
+    public void init() throws ServletException{
+        DiskFileItemFactory fileFactory = new DiskFileItemFactory();
+        File filesDir = (File) getServletContext().getAttribute("FILES_DIR_FILE");
+        fileFactory.setRepository(filesDir);
+        new ServletFileUpload(fileFactory);
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String fileName = request.getParameter("fileName");
+        if(fileName == null || fileName.equals("")){
+            throw new ServletException("File Name can't be null or empty");
+        }
+        File file = new File(request.getServletContext().getAttribute("FILES_DIR")+File.separator+fileName);
+        if(!file.exists()){
+            throw new ServletException("File doesn't exists on server.");
+        }
+        System.out.println("File location on server::"+file.getAbsolutePath());
+        ServletContext ctx = getServletContext();
+        InputStream fis = new FileInputStream(file);
+        String mimeType = ctx.getMimeType(file.getAbsolutePath());
+        response.setContentType(mimeType != null? mimeType:"application/octet-stream");
+        response.setContentLength((int) file.length());
+        response.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
+ 
+        ServletOutputStream os       = response.getOutputStream();
+        byte[] bufferData = new byte[1024];
+        int read=0;
+        while((read = fis.read(bufferData))!= -1){
+            os.write(bufferData, 0, read);
+        }
+        os.flush();
+        os.close();
+        fis.close();
+    }
 }
