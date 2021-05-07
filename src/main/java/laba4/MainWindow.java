8718d9f627917ca128fff189a6c129c495f369fb
Radmir Abkarimov <Abkarimov2001@outlook.com>
2021-05-07 19:49:33 +0500

    Проверка работы кнопки

--- src/main/java/laba4/MainWindow.java
+++ src/main/java/laba4/MainWindow.java
@@ -11,11 +11,14 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name="Calc", urlPatterns="/JavaCalc")
 public class MainWindow extends HttpServlet {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		RequestCalc Calc = RequestCalc.fromRequestParameters(request);
 		Calc.setAsRequestAttributesAndCalculate(request);
-		
-		request.getRequestDispatcher("/Form.jsp").forward(request, response);
 	}
 	
 	private static class RequestCalc {
