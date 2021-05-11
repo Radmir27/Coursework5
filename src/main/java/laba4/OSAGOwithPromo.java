a236fe70574a4fb31406f328c22f0e711e8491d2
Radmir Abkarimov <Abkarimov2001@outlook.com>
2021-05-11 21:23:19 +0500

    исправление

--- src/main/java/laba4/OSAGOwithPromo.java
+++ src/main/java/laba4/OSAGOwithPromo.java
@@ -11,7 +11,7 @@ public class OSAGOwithPromo extends OSAGO {
 	    	  promo = 1;
 	    	  }
 		cpr = 1;
-		int tbInt = (int) (((btMax - btMin)*staf+btMin)*ct*cbm*csv*cm*cpr*promo*100);
+		int tbInt = (int) (((btMax - btMin)*100*staf+btMin)*ct*cbm*csv*cm*cpr*promo);
 		float tb = tbInt/100;
 		return tb;
 	}
